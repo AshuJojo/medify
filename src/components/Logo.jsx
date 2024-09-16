d17b121dbe1a086c0be3e6d6a06a3488e27fe37b
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-09-16 14:21:32 +0530

    Implemented Responsive Navbar

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,17 @@
 import { Box, Typography } from "@mui/material"
 
-const Logo = () => {
+const Logo = ({ color = "primary", iconSize = 24 }) => {
   return (
     <Box sx={{
-        display: "flex",
-        justifyContent: 'start',
-        alignItems: 'center',
-        py: 2,
-        gap: 0.5,
-      }}>
-        <img src='/logo.svg' alt="Medify Logo" />
-        <Typography variant="h6" color="primary" sx={{ fontWeight: 700 }}>Medify</Typography>
-      </Box>
+      display: "flex",
+      justifyContent: 'start',
+      alignItems: 'center',
+      py: 2,
+      gap: 0.5,
+    }}>
+      <img src='/logo.svg' alt="Medify Logo" width={iconSize} height={iconSize}/>
+      <Typography variant="h6" color={color} sx={{ fontWeight: 700 }}>Medify</Typography>
+    </Box>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,28 @@
 
-import { Box, Button, Container, Typography, useTheme } from "@mui/material";
+import { Box, Button, Container, Divider, Drawer, Typography, useMediaQuery, useTheme } from "@mui/material";
 import Logo from "./Logo";
 import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
-const CustomNavLink = ({ to, children, borderBottom }) => {
-  const theme = useTheme();
+const CustomNavLink = ({ theme, to, children, borderBottom, width = "auto" }) => {
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
     <NavLink
       to={to}
       style={({ isActive }) => {
         return {
+          width: width,
           textDecoration: 'none',
           color: isActive ? theme.palette.primary.main : "black",
           fontWeight: isActive ? 500 : 400,
-          borderBottom: isActive && borderBottom === undefined ? `4px solid ${theme.palette.primary.main}` : "",
+          borderBottom: isActive && borderBottom === undefined && isLargeScreen ? `4px solid ${theme.palette.primary.main}` : "",
           paddingTop: 19,
           paddingBottom: 17,
           marginTop: -18,
-          marginBottom: -18
+          marginBottom: -18,
+          textAlign: 'center'
         };
       }}
     >
@@ -28,6 +32,49 @@ const CustomNavLink = ({ to, children, borderBottom }) => {
 }
 
 const Navbar = ({ bgColor = 'white', shadow = true }) => {
+  const theme = useTheme();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const openDrawer = () => {
+    console.log("Open Drawer Clicked");
+    setIsDrawerOpen(true)
+  }
+
+  const drawer = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={() => setIsDrawerOpen(false)}>
+      <Container sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        mt: 2,
+        flexDirection: 'column'
+      }}>
+        <CustomNavLink to="/" borderBottom={false} theme={theme}><Logo /></CustomNavLink>
+
+        <Divider sx={{ width: '100%' }} />
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          gap: 2,
+          mt: 2
+        }}>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Find Doctors</Typography>
+          <CustomNavLink to="/hospitals" theme={theme}><Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }} >Hospital</Typography></CustomNavLink>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Medicines</Typography>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Surgeries</Typography>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Software for Provider</Typography>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Facilities</Typography>
+          <CustomNavLink to="/mybookings" borderBottom={false} theme={theme} width="100%">
+            <Button sx={{ width: '100%' }}>
+              My Bookings
+            </Button>
+          </CustomNavLink>
+        </Box>
+      </Container >
+    </Box>
+  );
 
   return (
     <Box
@@ -38,23 +85,32 @@ const Navbar = ({ bgColor = 'white', shadow = true }) => {
       }}
     >
       <Container sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <CustomNavLink to="/" borderBottom={false}><Logo /></CustomNavLink>
+        <CustomNavLink to="/" borderBottom={false} theme={theme}><Logo /></CustomNavLink>
 
         <Box sx={{
-          display: "flex",
+          display: { xs: 'none', md: "flex" },
           justifyContent: 'flex-end',
           alignItems: 'center',
-          gap: 4
+          gap: { md: 2, lg: 4 },
         }}>
-          <Typography to="#">Find Doctors</Typography>
-          <CustomNavLink to="/hospitals">Hospitals</CustomNavLink>
-          <Typography to="#">Medicines</Typography>
-          <Typography to="#">Surgeries</Typography>
-          <Typography to="#">Software for Provider</Typography>
-          <Typography to="#">Facilities</Typography>
-          <CustomNavLink to="/mybookings" borderBottom={false}><Button>My Bookings</Button></CustomNavLink>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Find Doctors</Typography>
+          <CustomNavLink to="/hospitals" theme={theme}><Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }} >Hospital</Typography></CustomNavLink>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Medicines</Typography>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Surgeries</Typography>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Software for Provider</Typography>
+          <Typography to="#" sx={{ typography: { sm: 'body1', xs: 'body2' }, cursor: 'pointer' }}>Facilities</Typography>
+          <CustomNavLink to="/mybookings" borderBottom={false} theme={theme}><Button>My Bookings</Button></CustomNavLink>
+        </Box>
+        <Box sx={{
+          display: { xs: 'flex', md: 'none' },
+          alignItems: 'center'
+        }}>
+          <GiHamburgerMenu fontSize={30} color={theme.palette.primary.main} onClick={openDrawer} />
         </Box>
       </Container >
+      <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+        {drawer}
+      </Drawer>
     </Box >
   )
 }
